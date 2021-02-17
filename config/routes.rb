7010f1618b7ac370c4d37ef1ea419b46ec9f6f88
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-02-17 16:42:45 +0000

    challenge 2 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'restaurants/index'
-  get 'restaurants/show'
-  get 'restaurants/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show, :new, :create] do
-    restaurants :reviews, only: [:new, :create, :delete]
+    resources :reviews, only: [:new, :create]
   end
 end
