c53d4998230b1941441d6403200acb7bfa860ca3
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-02-17 15:00:06 +0000

    adding index, new & create views

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,24 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    # Will raise ActiveModel::ForbiddenAttributesError
+    redirect_to restaurants_index_path
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
