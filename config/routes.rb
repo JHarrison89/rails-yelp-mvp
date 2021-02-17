1a1ebc76db57432c3be28f80ba09bbb0d9692f9f
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-02-17 15:22:29 +0000

    adding reviews to routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get 'restaurants/show'
   get 'restaurants/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants, only: [:index, :show, :new, :create]
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    restaurants :reviews, only: [:new, :create, :delete]
+  end
 end
