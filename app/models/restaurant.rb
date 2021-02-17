1b397ff508d5eed285bd3dfd2ad8703bb821fa18
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-02-17 13:15:01 +0000

    changing restaurant phone number to string

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,8 @@
 class Restaurant < ApplicationRecord
-  validates :category, inclusion: { in: %w[small medium large],
-    message: "%{value} is not a category" }
+  validates :category, inclusion: { in: %w[chinese italian japanese french belgian],
+  message: "%{value} is not a category" }
+  validates :phone_number, presence: true
+  validates :name, presence: true
+  validates :address, presence: true
+  has_many :reviews, dependent: :destroy
 end
