601d62292f7f1e03d4d0360b253f88c082ad26cb
Jeremaia Harrison <jeremaia.harrison@gmail.com>
2021-02-17 15:03:34 +0000

    adding show view

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   get 'restaurants/index'
   get 'restaurants/show'
   get 'restaurants/new'
-  get 'games/index'
-  get 'games/show'
-  get 'games/new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:index, :show, :new, :create]
 end
